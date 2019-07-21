ccb3086aa04ba6454638cae8eefff8e2da63c8d1
Luke Hopkins <lhopki01@gmail.com>
2019-07-21 11:15:19 +0100

    remove unused css

--- static/js/app.js
+++
@@ -1,8 +0,0 @@
-// Enable any popovers
-$(function () {
-    $('[data-toggle="popover"]').popover()
-    $('[data-toggle="tooltip"]').tooltip()
-    var Moment = moment();
-    var pld = $('#pageloaddate')
-    pld.html(Moment.format('ddd D MMM HH:mm:ss'))
-})
